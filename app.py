da709be89b5de009603b91f8d94f127802dba56d
William C <56387759+williamchnt@users.noreply.github.com>
2022-06-17 09:17:16 +0200

    first comit

---
+++ app.py
@@ -0,0 +1,33 @@
+from requests import Request, Session, Response
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<lat>/<lon>')
+def get_weather(lat, lon):
+    try:
+        key = os.environ['API_KEY']
+        url = 'http://api.openweathermap.org/data/2.5/weather'
+        params = {
+            'lat': lat,
+            'lon': lon,
+            'appid': key
+        }
+        s = Session()
+        req = Request('GET', url, params=params)
+        prepped = req.prepare()
+        resp = s.send(prepped)
+        return resp.json()
+    except Exception as e:
+        return e
+
+if __name__ == "__main__":
+       app.run(host='0.0.0.0')
+
+
+
+# lat = os.environ['LAT']
+# lon = os.environ['LONG']
+
+# print(get_weather(lat,lon,key))
